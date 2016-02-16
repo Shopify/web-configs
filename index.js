a5a130b8645f02d4ba0a70be73581e0061483760
Chris Sauve <chris.sauve@shopify.com>
2016-02-16 15:43:15 -0500

    Upgrade ESLint

--- index.js
+++ index.js
@@ -3,9 +3,4 @@ module.exports = {
     'require-flow': require('./lib/rules/require-flow'),
     'binary-assignment-parens': require('./lib/rules/binary-assignment-parens'),
   },
-
-  rulesConfig: {
-    'require-flow': 0,
-    'binary-assignment-parens': 0,
-  },
 };
