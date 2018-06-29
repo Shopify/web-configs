dd4ead43ad86033b4b5dc7848845213e995384ea
Caleb St-Denis <caleb.stdenis@shopify.com>
2018-06-29 15:26:33 -0400

    Change no-content rule to allow keywords, disallowing only strings
    Blacklist non-empty strings instead of whitelisting empty strings.

--- plugins/no-content/no-content.test.js
+++ plugins/no-content/no-content.test.js
@@ -4,25 +4,35 @@ const shopifyi18n = require('.');
 testRule(shopifyi18n.rule, {
   ruleName: shopifyi18n.ruleName,
   config: true,
+  skipBasicChecks: true,
 
   reject: [
     {
-      code: ".foo {content: 'nope, no i18n here'}",
+      code: ".foo::before {content: 'nope, no i18n here' }",
     },
     {
-      code: '.foo {content: "none here either"}',
+      code: '.foo::before {content: "none here either" }',
+    },
+    {
+      code: ".foo::before {content: counter(ordered-counter, decimal) '.' }",
     },
   ],
 
   accept: [
     {
-      code: ".foo { content: '' }",
+      code: ".foo::before { content: '' }",
     },
     {
-      code: '.foo { content: "" }',
+      code: '.foo::before { content: "" }',
     },
     {
-      code: '.foo { content: none }',
+      code: '.foo::before {content: counter(ordered-counter, decimal) }',
+    },
+    {
+      code: '.foo::before { content: none }',
+    },
+    {
+      code: '.foo::before { content: open-quote }',
     },
   ],
 });
