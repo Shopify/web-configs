0f9e50c4d98713adbb11dabf9870fcba1975014c
Ben Scott <ben.scott@shopify.com>
2018-07-12 14:31:38 -0700

    Use stylelint-prettier in prettier config
    stylelint-prettier runs prettier transforms as a stylelint plugin. This
    means that you can do `stylelint foo.css --fix` and format the file
    as prettier would.
    
    This removes the need for having to run (s)css files through prettier
    then through stylelint to get fully formatted output (such as using
    prettier-stylelint-formatter). This is in line with how our eslint
    prettier config allows us to run `eslint foo.js --fix` to prettify the
    content as part of an eslint rule.

--- prettier.js
+++ prettier.js
@@ -1,11 +1,14 @@
 module.exports = {
+  plugins: [
+    'stylelint-prettier',
+  ],
   extends: [
     './index',
     'stylelint-config-prettier',
   ],
-
-  // conflicts with prettier formatting
   rules: {
+    'prettier/prettier': true,
+    // conflicts with prettier formatting
     'scss/double-slash-comment-empty-line-before': null,
     'scss/operator-no-newline-after': null,
   },
