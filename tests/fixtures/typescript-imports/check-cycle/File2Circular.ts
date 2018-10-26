afd3c2740494c5709a604782ef6bb8198f0cf7b5
Ben Scott <ben.scott@shopify.com>
2018-10-26 13:07:45 -0700

    Set up import parsers so import/* rules work in typescript
    It turns out you need some extra config in order for some of the import
    checking rules to do their thing with TypeScript.

---
+++ tests/fixtures/typescript-imports/check-cycle/File2Circular.ts
@@ -0,0 +1,4 @@
+import {File1} from './index';
+
+const result = File1 + 33;
+export default result;
---
+++ tests/fixtures/typescript-imports/check-cycle/index.ts
@@ -0,0 +1,3 @@
+export {default as File1} from './File1';
+export {default as File2Circular} from './File2Circular';
+export {default as File3NoCircular} from './File3NoCircular';
