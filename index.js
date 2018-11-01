e2f39afd1a4890a1db7fbb2456d4b84913eda3ae
Matt Seccafien <matt.seccafien@shopify.com>
2018-11-01 16:25:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = [
   'last 3 chrome versions',
+  'last 3 chromeandroid versions',
   'last 3 firefox versions',
-  'last 2 versions',
-  'safari >= 8',
-  'ios >= 8',
-  'ie >= 11',
-  'explorermobile >= 11',
+  'last 3 opera versions',
+  'last 2 edge versions',
+  'safari >= 10',
+  'ios >= 10',
   'android >= 4.4',
 ];
