7a800edb436993fe234c06eb0714252bc2bfc745
Ben Scott <227292+BPScott@users.noreply.github.com>
2019-12-16 11:58:35 -0800

    Update dependencies and add extra rules (#54)

--- rules/scss.js
+++ rules/scss.js
@@ -15,14 +15,22 @@ module.exports = {
   'scss/at-if-closing-brace-space-after': 'always-intermediate',
   // Disallow leading underscore in partial names in @import.
   'scss/at-import-no-partial-leading-underscore': true,
+  // Disallow unneeded `!= null` in if expressions (`@if $x != null` can be simplified to `@if $x`)
+  'scss/at-if-no-null': true,
   // Specify blacklist of disallowed file extensions for partial names in @import commands.
   'scss/at-import-partial-extension-blacklist': ['scss'],
   // Specify whitelist of allowed file extensions for partial names in @import commands.
   'scss/at-import-partial-extension-whitelist': null,
+  // Require using `@each $key, $value in $list` instead of getting the value inside the loop
+  'scss/at-each-key-value-single-line': true,
   // Disallow parentheses in argumentless @mixin calls.
   'scss/at-mixin-argumentless-call-parentheses': 'never',
   // Specify a pattern for Sass/SCSS-like mixin names.
   'scss/at-mixin-pattern': null,
+  // Disalow parentheses around at directives calls (`@if ($x)` can be simplified to `@if $x`   )
+  'scss/at-rule-conditional-no-parentheses': true,
+  // Disallow using string interpolation to build numbers with units (`#{x}px` should be `$x * 1px`)
+  'scss/dimension-no-non-numeric-values': true,
   // Require a newline after the colon in $-variable declarations.
   'scss/dollar-variable-colon-newline-after': null,
   // Require a single space or disallow whitespace after the colon in $-variable declarations.
@@ -51,6 +59,8 @@ module.exports = {
   'scss/declaration-nested-properties': 'never',
   // Disallow nested properties of the same "namespace" be divided into multiple groups.
   'scss/declaration-nested-properties-no-divided-groups': true,
+  // Disallow unquoted strings inside the unquote function
+  'scss/function-unquote-no-unquoted-strings-inside': true,
   //  Require a media feature value be a $-variable or disallow $-variables in media feature values.
   'scss/media-feature-value-dollar-variable': null,
   // Disallow linebreaks after Sass operators.
@@ -63,8 +73,12 @@ module.exports = {
   'scss/partial-no-import': true,
   // Disallow redundant nesting selectors (`&`).
   'scss/selector-no-redundant-nesting-selector': true,
+  // Disallow union class names  (e.g. `&-foo`)
+  'scss/selector-no-union-class-name': true,
   // Disallow dollar variables within a stylesheet.
   'scss/no-dollar-variables': null,
   // Disallow duplicate dollar variables within a stylesheet.
   'scss/no-duplicate-dollar-variables': null,
+  // Disallow duplicate mixis within a stylesheet
+  'scss/no-duplicate-mixins': true,
 };
