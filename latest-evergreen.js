f4a27ae073c7aaba0e050de56649ea04eebf1380
Alexandre Côté <alex.cote@shopify.com>
2020-03-19 11:09:33 -0400

    Only support last major version of safari and ios on evergreen bundle (#8)

--- latest-evergreen.js
+++ latest-evergreen.js
@@ -4,6 +4,6 @@ module.exports = [
   'last 1 firefox versions',
   'last 1 opera versions',
   'last 1 edge versions',
-  'safari >= 11',
-  'ios >= 11',
+  'last 1 safari major versions',
+  'last 1 ios major versions',
 ];
