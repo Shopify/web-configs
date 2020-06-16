6d7ddffca5661f74ea210375e468b2c4de8132aa
Brandon Yetman <brandon.yetman@shopify.com>
2020-06-16 10:26:31 -0400

    add 'graphql/required-fields' rule for 'id' field and test

--- packages/eslint-plugin/tests/lib/config/graphql.test.js
+++ packages/eslint-plugin/tests/lib/config/graphql.test.js
@@ -12,6 +12,16 @@ describe('config', () => {
       ).toMatch(/Cannot query field .*DOES_NOT_EXIST/);
     }, 8000);
 
+    it('validates .graphql files with required `id` field using graphql plugin', () => {
+      expect(
+        execESLint(
+          `--ext .graphql --ignore-pattern "**/graphql-lint-error/build/*" --config "${configFile(
+            'graphql',
+          )}" "${fixtureFile('graphql-lint-error')}"`,
+        ),
+      ).toMatch(/'id' field required on 'foo'/);
+    }, 8000);
+
     it('validates .graphql file syntax using graphql plugin', () => {
       expect(
         execESLint(
