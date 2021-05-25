dc3a25356e91a05b934239f88583c7d97abe7a18
Victor Sumner <victor.sumner@shopify.com>
2021-05-25 14:00:26 -0400

    fix common

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   targets: 'current node',
-  presets: [['@shopify/babel-preset/common', {modules: 'commonjs'}]],
+  presets: [['@shopify/babel-preset', {modules: 'commonjs'}]],
 };
