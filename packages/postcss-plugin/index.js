fe8c220f5b6f9e621248c299ab5f05dc079fbe0c
Victor Sumner <victor.sumner@shopify.com>
2021-07-07 12:12:25 -0400

    Refactor options

--- packages/postcss-plugin/index.js
+++ packages/postcss-plugin/index.js
@@ -1,12 +1,12 @@
 const postcss = require('postcss');
 
-module.exports = (options = {}) => {
+module.exports = ({calcOptions = {}, autopprefixerOptions = {}}) => {
   return {
     ...postcss([
-      require('postcss-calc')(options),
+      require('postcss-calc')(calcOptions),
       require('postcss-flexbugs-fixes'),
       require('postcss-will-change'),
-      require('autoprefixer')(options),
+      require('autoprefixer')(autopprefixerOptions),
     ]),
     postcssPlugin: '@shopify/postcss-plugin',
   };
