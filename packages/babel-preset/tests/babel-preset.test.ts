7d6bdae252222f45f41892d7ced9b546cca33f12
Ben Scott <ben.scott@shopify.com>
2021-07-27 12:41:25 -0700

    test output of transformed code

--- packages/babel-preset/tests/babel-preset.test.ts
+++ packages/babel-preset/tests/babel-preset.test.ts
@@ -6,15 +6,24 @@ describe('babel-preset-e2e-test', () => {
   describe('legacy decorator tests', () => {
     it('transforms code without errors or warnings', async () => {
       const {code} = transform(
-        testContent,
+        sampleCode,
         configFactory({presetOptions: {typescript: true}}),
       );
-      expect(code).toBe(testContentOutput.trim());
+      expect(code).toBe(expectedTransformedSampleCode.trim());
+
+      // The test content must contain a function called `Test`, so that
+      // we can call it and get the result of running that code
+      // eslint-disable-next-line no-new-func
+      const sampleCodeExecutionResult = Function(`${code};\nreturn Test()`)();
+
+      expect(sampleCodeExecutionResult).toStrictEqual({
+        decoratorOutput: 'TEST',
+      });
     });
 
     it('throws an error when encountering invalid code, if decorator transforms are not enabled', async () => {
       expect(() => {
-        transform(testContent, configFactory());
+        transform(sampleCode, configFactory());
       }).toThrow('Unexpected token');
     });
   });
@@ -37,52 +46,59 @@ function configFactory({
   };
 }
 
-const testContent = `
-function uppercase() {
-  return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    descriptor.value = function(...args: any[]) {
-      return originalMethod.apply(this, args).toUpperCase();
+const sampleCode = `
+function Test() {
+  function uppercase() {
+    return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
+      const originalMethod = descriptor.value;
+      descriptor.value = function(...args: any[]) {
+        return originalMethod.apply(this, args).toUpperCase();
+      };
+      return descriptor;
     };
-    return descriptor;
   };
-};
 
-class ExampleClass {
-  @uppercase()
-  method() {
-    return 'test';
+  class ExampleClass {
+    @uppercase()
+    method() {
+      return 'test';
+    }
   }
+  return {
+    decoratorOutput: new ExampleClass().method(),
+  };
 }
-
-console.log(new ExampleClass().method());
 `;
 
-const testContentOutput = `
+const expectedTransformedSampleCode = `
 "use strict";
-
-var _dec, _class;
 
 function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
 
-function uppercase() {
-  return function (_target, _propertyKey, descriptor) {
-    const originalMethod = descriptor.value;
+function Test() {
+  var _dec, _class;
 
-    descriptor.value = function (...args) {
-      return originalMethod.apply(this, args).toUpperCase();
+  function uppercase() {
+    return function (_target, _propertyKey, descriptor) {
+      const originalMethod = descriptor.value;
+
+      descriptor.value = function (...args) {
+        return originalMethod.apply(this, args).toUpperCase();
+      };
+
+      return descriptor;
     };
-
-    return descriptor;
-  };
-}
-
-;
-let ExampleClass = (_dec = uppercase(), (_class = class ExampleClass {
-  method() {
-    return 'test';
   }
 
-}, (_applyDecoratedDescriptor(_class.prototype, "method", [_dec], Object.getOwnPropertyDescriptor(_class.prototype, "method"), _class.prototype)), _class));
-console.log(new ExampleClass().method());
+  ;
+  let ExampleClass = (_dec = uppercase(), (_class = class ExampleClass {
+    method() {
+      return 'test';
+    }
+
+  }, (_applyDecoratedDescriptor(_class.prototype, "method", [_dec], Object.getOwnPropertyDescriptor(_class.prototype, "method"), _class.prototype)), _class));
+  return {
+    decoratorOutput: new ExampleClass().method()
+  };
+}
 `;
