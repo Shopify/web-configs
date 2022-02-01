1aee40a023b5b1f088e8ca23242a22259549cfb7
Roy Lee <51924945+royleejr@users.noreply.github.com>
2022-02-01 15:14:32 -0500

    Remove lines around comment rule in prettier eslint-plugins (#319)
    * Remove lines-around-comment rule in prettier eslint-plugin
    
    * Add removal of rule into the changelog

--- packages/eslint-plugin/lib/config/prettier.js
+++ packages/eslint-plugin/lib/config/prettier.js
@@ -13,15 +13,6 @@ module.exports = {
     'prefer-arrow-callback': 'off',
     'arrow-body-style': 'off',
 
-    // Special rule for 'lines-around-comment'
-    // https://github.com/prettier/eslint-config-prettier/blob/984de70e8c6b57684b444283561019389ccebd11/README.md#lines-around-comment
-    'lines-around-comment': [
-      'error',
-      {
-        beforeBlockComment: true,
-      },
-    ],
-
     // Special rule for 'no-unexpected-multiline'
     // https://github.com/prettier/eslint-config-prettier/blob/5399175c37466747aae9d407021dffec2c169c8b/README.md#no-unexpected-multiline
     'no-unexpected-multiline': 'error',
