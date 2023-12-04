4fd9e991d2d94252210890c8f6af06902099cac4
Jason Addleman <jason.addleman@shopify.com>
2023-12-04 14:22:18 -0500

    Use Record instead of index signatures
    Fix failures

--- packages/typescript-configs/definitions/styles.d.ts
+++ packages/typescript-configs/definitions/styles.d.ts
@@ -1,9 +1,9 @@
 declare module '*.scss' {
-  const classNames: {[key: string]: string};
+  const classNames: Record<string, string>;
   export default classNames;
 }
 
 declare module '*.css' {
-  const classNames: {[key: string]: string};
+  const classNames: Record<string, string>;
   export default classNames;
 }
