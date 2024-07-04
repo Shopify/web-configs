fdd8cd6dffa65a0c70922144f3bb03776eb1fc1b
Kyle Durand <kyle.durand@shopify.com>
2024-07-04 10:21:21 -0400

    chore: remove strip-ansi, use github formatter

--- packages/stylelint-plugin/tests/config-e2e.test.js
+++ packages/stylelint-plugin/tests/config-e2e.test.js
@@ -2,8 +2,6 @@ const {spawnSync} = require('child_process');
 const path = require('path');
 const {resolve} = require('path');
 
-const stripAnsi = require('strip-ansi');
-
 /**
  * Tests that report errors in multiple files may change the order of the files
  * across multiple runs.
@@ -15,14 +13,10 @@ describe('stylelint-plugin E2E Tests', () => {
     const result = runStylelint('value-keyword-case.*.scss');
 
     const expectedResult = `
-value-keyword-case.invalid.scss
-  1:7   ✖  Expected "Value" to be "value"    value-keyword-case
-  2:7   ✖  Expected "VALUE" to be "value"    value-keyword-case
-  5:10  ✖  Expected "Monaco" to be "monaco"  value-keyword-case
-  6:18  ✖  Expected "Monaco" to be "monaco"  value-keyword-case
-
-✖ 4 problems (4 errors, 0 warnings)
-  4 errors potentially fixable with the "--fix" option.
+::error file=value-keyword-case.invalid.scss,line=1,col=7,endLine=1,endColumn=8,title=Stylelint problem::Expected "Value" to be "value" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
+::error file=value-keyword-case.invalid.scss,line=2,col=7,endLine=2,endColumn=8,title=Stylelint problem::Expected "VALUE" to be "value" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
+::error file=value-keyword-case.invalid.scss,line=5,col=10,endLine=5,endColumn=11,title=Stylelint problem::Expected "Monaco" to be "monaco" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
+::error file=value-keyword-case.invalid.scss,line=6,col=18,endLine=6,endColumn=19,title=Stylelint problem::Expected "Monaco" to be "monaco" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
     `.trim();
 
     expect(result.error).toStrictEqual(expectedResult);
@@ -36,15 +30,11 @@ value-keyword-case.invalid.scss
     // trailing whitespace and editors really want to remove that trailing
     // whitespace when saving the file
     const expectedResult = `
-scss.invalid.scss
-   6:5   ✖  Expected ".n3" to have no more than 2 classes                                                          selector-max-class
-   6:5   ✖  Expected ".n3" to have no more than 1 combinator                                                       selector-max-combinators
-  16:12  ✖  Expected "$value * 1px" instead of "#{$value}px". Consider writing "value" in terms of px originally.  scss/dimension-no-non-numeric-values
-  22:3   ✖  Unexpected union class name with the parent selector (&)                                               scss/selector-no-union-class-name
-
-✖ 4 problems (4 errors, 0 warnings)
+::error file=scss.invalid.scss,line=6,col=5,endLine=6,endColumn=8,title=Stylelint problem::Expected ".n3" to have no more than 2 classes (selector-max-class) - https://stylelint.io/user-guide/rules/selector-max-class
+::error file=scss.invalid.scss,line=6,col=5,endLine=6,endColumn=8,title=Stylelint problem::Expected ".n3" to have no more than 1 combinator (selector-max-combinators) - https://stylelint.io/user-guide/rules/selector-max-combinators
+::error file=scss.invalid.scss,line=16,col=12,endLine=16,endColumn=22,title=Stylelint problem::Expected "$value * 1px" instead of "#{$value}px". Consider writing "value" in terms of px originally. (scss/dimension-no-non-numeric-values) - https://github.com/stylelint-scss/stylelint-scss/blob/master/src/rules/dimension-no-non-numeric-values
+::error file=scss.invalid.scss,line=22,col=3,endLine=22,endColumn=8,title=Stylelint problem::Unexpected union class name with the parent selector (&) (scss/selector-no-union-class-name) - https://github.com/stylelint-scss/stylelint-scss/blob/master/src/rules/selector-no-union-class-name
     `.trim();
-
     expect(result.error).toStrictEqual(expectedResult);
     expect(result.status).toBe(2);
   });
@@ -53,13 +43,16 @@ scss.invalid.scss
 function runStylelint(pattern) {
   const stylelintCmd = resolve(__dirname, `../node_modules/.bin/stylelint`);
 
-  const result = spawnSync(stylelintCmd, [pattern], {
+  const result = spawnSync(stylelintCmd, ['--formatter=github', pattern], {
     cwd: path.resolve(__dirname, 'fixtures'),
   });
 
   return {
     status: result.status,
-    output: stripAnsi(result.stdout.toString()).trim(),
-    error: stripAnsi(result.stderr.toString()).trim(),
+    output: result.stdout.toString().trim(),
+    error: result.stderr
+      .toString()
+      .trim()
+      .replace(/file=.*?fixtures\//g, 'file='),
   };
 }
