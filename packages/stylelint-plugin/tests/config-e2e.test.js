4ddaaab960ed972b822eb5da44dd0d3a1c5777ac
Ben Scott <ben.scott@shopify.com>
2024-09-09 12:48:36 -0700

    Adjust stylelint e2e tests
    Avoid using the now-deprecated github formatter. Instead use the json
    format and transform it into something that looks like the github
    formatter.
    
    Update dev version of stylelint to account for the new version now
    supporting endColumn in a few of the rules we test.

--- packages/stylelint-plugin/tests/config-e2e.test.js
+++ packages/stylelint-plugin/tests/config-e2e.test.js
@@ -1,6 +1,5 @@
 const {spawnSync} = require('child_process');
-const path = require('path');
-const {resolve} = require('path');
+const {resolve, relative} = require('path');
 
 /**
  * Tests that report errors in multiple files may change the order of the files
@@ -13,10 +12,10 @@ describe('stylelint-plugin E2E Tests', () => {
     const result = runStylelint('value-keyword-case.*.scss');
 
     const expectedResult = `
-::error file=value-keyword-case.invalid.scss,line=1,col=7,endLine=1,endColumn=8,title=Stylelint problem::Expected "Value" to be "value" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
-::error file=value-keyword-case.invalid.scss,line=2,col=7,endLine=2,endColumn=8,title=Stylelint problem::Expected "VALUE" to be "value" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
-::error file=value-keyword-case.invalid.scss,line=5,col=10,endLine=5,endColumn=11,title=Stylelint problem::Expected "Monaco" to be "monaco" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
-::error file=value-keyword-case.invalid.scss,line=6,col=18,endLine=6,endColumn=19,title=Stylelint problem::Expected "Monaco" to be "monaco" (value-keyword-case) [maybe fixable] - https://stylelint.io/user-guide/rules/value-keyword-case
+::error file=value-keyword-case.invalid.scss,line=1,col=7,endLine=1,endColumn=12,title=Stylelint problem::Expected "Value" to be "value" (value-keyword-case)
+::error file=value-keyword-case.invalid.scss,line=2,col=7,endLine=2,endColumn=12,title=Stylelint problem::Expected "VALUE" to be "value" (value-keyword-case)
+::error file=value-keyword-case.invalid.scss,line=5,col=10,endLine=5,endColumn=16,title=Stylelint problem::Expected "Monaco" to be "monaco" (value-keyword-case)
+::error file=value-keyword-case.invalid.scss,line=6,col=18,endLine=6,endColumn=24,title=Stylelint problem::Expected "Monaco" to be "monaco" (value-keyword-case)
     `.trim();
 
     expect(result.error).toStrictEqual(expectedResult);
@@ -30,10 +29,10 @@ describe('stylelint-plugin E2E Tests', () => {
     // trailing whitespace and editors really want to remove that trailing
     // whitespace when saving the file
     const expectedResult = `
-::error file=scss.invalid.scss,line=6,col=5,endLine=6,endColumn=8,title=Stylelint problem::Expected ".n3" to have no more than 2 classes (selector-max-class) - https://stylelint.io/user-guide/rules/selector-max-class
-::error file=scss.invalid.scss,line=6,col=5,endLine=6,endColumn=8,title=Stylelint problem::Expected ".n3" to have no more than 1 combinator (selector-max-combinators) - https://stylelint.io/user-guide/rules/selector-max-combinators
-::error file=scss.invalid.scss,line=16,col=12,endLine=16,endColumn=22,title=Stylelint problem::Expected "$value * 1px" instead of "#{$value}px". Consider writing "value" in terms of px originally. (scss/dimension-no-non-numeric-values) - https://github.com/stylelint-scss/stylelint-scss/blob/master/src/rules/dimension-no-non-numeric-values
-::error file=scss.invalid.scss,line=22,col=3,endLine=22,endColumn=8,title=Stylelint problem::Unexpected union class name with the parent selector (&) (scss/selector-no-union-class-name) - https://github.com/stylelint-scss/stylelint-scss/blob/master/src/rules/selector-no-union-class-name
+::error file=scss.invalid.scss,line=16,col=12,endLine=16,endColumn=22,title=Stylelint problem::Expected "$value * 1px" instead of "#{$value}px". Consider writing "value" in terms of px originally. (scss/dimension-no-non-numeric-values)
+::error file=scss.invalid.scss,line=22,col=3,endLine=22,endColumn=8,title=Stylelint problem::Unexpected union class name with the parent selector (&) (scss/selector-no-union-class-name)
+::error file=scss.invalid.scss,line=6,col=5,endLine=6,endColumn=8,title=Stylelint problem::Expected ".n3" to have no more than 2 classes (selector-max-class)
+::error file=scss.invalid.scss,line=6,col=5,endLine=6,endColumn=8,title=Stylelint problem::Expected ".n3" to have no more than 1 combinator (selector-max-combinators)
     `.trim();
     expect(result.error).toStrictEqual(expectedResult);
     expect(result.status).toBe(2);
@@ -41,18 +40,34 @@ describe('stylelint-plugin E2E Tests', () => {
 });
 
 function runStylelint(pattern) {
+  const stylelintCwd = resolve(__dirname, 'fixtures');
   const stylelintCmd = resolve(__dirname, `../node_modules/.bin/stylelint`);
 
-  const result = spawnSync(stylelintCmd, ['--formatter=github', pattern], {
-    cwd: path.resolve(__dirname, 'fixtures'),
+  const result = spawnSync(stylelintCmd, ['--formatter=json', pattern], {
+    cwd: stylelintCwd,
   });
+
+  const jsonErrors = JSON.parse(result.stderr.toString().trim());
+
+  const errorLines = [];
+
+  for (const error of jsonErrors) {
+    for (const warning of error.warnings) {
+      errorLines.push(
+        `::error file=${relative(stylelintCwd, error.source)}` +
+          `,line=${warning.line}` +
+          `,col=${warning.column}` +
+          `,endLine=${warning.endLine}` +
+          `,endColumn=${warning.endColumn}` +
+          `,title=Stylelint problem` +
+          `::${warning.text}`,
+      );
+    }
+  }
 
   return {
     status: result.status,
     output: result.stdout.toString().trim(),
-    error: result.stderr
-      .toString()
-      .trim()
-      .replace(/file=.*?fixtures\//g, 'file='),
+    error: errorLines.join('\n'),
   };
 }
